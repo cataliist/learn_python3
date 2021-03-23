78547ea3ab07af503a92d55c452225aa902ccd58
genique <cataliistceo@gmail.com>
2021-03-23 14:28:00 +0530

    firstScript updated

---
+++ firstScript.py
@@ -0,0 +1,12 @@
+movies = [
+    "The Holy Grail",
+    "THe Life of Brian",
+    "The Meaning of Life"
+]
+
+movies.insert(0,"Harry Potter")
+movies.append("Holy Sleptover")
+movies.remove("Holy Sleptover")
+movies.pop(0)
+
+print(movies)
