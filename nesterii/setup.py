a8e3e52d1ac9ccb4d91939a37a8683e8c398f332
genique <cataliistceo@gmail.com>
2021-03-23 23:37:45 +0530

    module distribution

---
+++ nesterii/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+
+    name            = 'nesterii',
+    version         = '1.0.0',
+    py_modules      = ['nesterii'],
+    author          = 'cataliist',
+    author_email    = 'cataliistceo@gmail.com',
+    url             = 'http://www.cataliist.com',
+    description     = 'a simple printer for nested lists',
+)
