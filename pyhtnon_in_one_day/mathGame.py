bb9ab7d881656558faab6507fa5e9becab16ed13
genique <cataliistceo@gmail.com>
2021-03-31 14:13:49 +0530

    interactive math game

--- pyhtnon_in_one_day/mathGame.py
+++ pyhtnon_in_one_day/mathGame.py
@@ -6,11 +6,18 @@ try:
 
     userName = input("Enter your user name: ")
     
-    if getUserPoint(userName) == -1:
+    if getUserPoint(userName) == "-1":
         newUser = True
+    else:
+        newUser = False    
 
-    userChoice = 0
-        
+    userChoice = ''
+    userScore = 0
+    while userChoice != "-1":
+        userScore = userScore + generateQuestion()
+        userChoice = input("Enter -1 to exit the game or press any key to continue: ")
+    
+    updateUserPoints(newUser,userName,str(userScore))         
 
 except Exception as err:
     print("Unknown Error: " + str(err))
