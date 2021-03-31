e496ef033da013f336dfa8524f8df97d0c339f80
genique <cataliistceo@gmail.com>
2021-03-31 13:01:09 +0530

    interactive math game

--- pyhtnon_in_one_day/mathGame.py
+++ pyhtnon_in_one_day/mathGame.py
@@ -1,6 +1,16 @@
-import os
-import sys
-sys.path.append("modules")
-import myPythonFunctions
+try:
+    import os
+    import sys
+    sys.path.append("modules")
+    from myPythonFunctions import getUserPoint, updateUserPoints, generateQuestion
 
-print(myPythonFunctions.getUserPoint("cataliister"))
+    userName = input("Enter your user name: ")
+    
+    if getUserPoint(userName) == -1:
+        newUser = True
+
+    userChoice = 0
+        
+
+except Exception as err:
+    print("Unknown Error: " + str(err))
--- pyhtnon_in_one_day/modules/myPythonFunctions.py
+++ pyhtnon_in_one_day/modules/myPythonFunctions.py
@@ -8,10 +8,87 @@ def getUserPoint(userName):
         with open("userScores.txt","r") as userFile:
             userDict = {}
             for each in userFile:
-                (name, score) = each.strip().split("\t")
+                (name, score) = each.split(",")
                 userDict[name] = score
-            if userDict[userName]:
+            if userName in userDict.keys():
                 return(userDict[userName])
             return("-1")    
     except Exception as err:
         return(err)            
+
+
+
+def updateUserPoints(newUser, userName, userScore):
+    if newUser:
+        try:
+            with open("userScores.txt","a") as userFile:
+                userFile.write(userName + "," + userScore + "\n")
+        except Exception as err:
+            return(err)        
+    else:
+        try:
+            with open("userScores.tmp","w") as userFileTmp:
+                with open("userScores.txt","r") as userFile:
+                    for each in userFile:
+                        (name,score) = each.strip().split(",")
+                        if name == userName:
+                            userFileTmp.write(name + "," + userScore + "\n")
+                        else:
+                            userFileTmp.write(name + "," + score + "\n")
+            os.remove("userScores.txt")
+            os.rename("userScores.tmp","userScores.txt")                    
+        except Exception as err:
+            return(err)                    
+
+
+
+def generateQuestion():
+    
+    operandList = [0,0,0,0,0]
+    operatorList = ['','','','']
+    operatorDict = {
+        1:"+",
+        2:"-",
+        3:"**",
+        4:"*"
+    }
+
+    for each in range(0,5):
+        operandList[each] = random.randint(1,9)
+
+    counter = 0
+    while counter < 4:
+        randomInt = random.randint(1,4)
+        if counter > 0 and operatorDict[randomInt] == "**" and operatorList[counter-1] == "**":
+            operatorList[counter] = operatorDict[1]
+        else:
+            operatorList[counter] = operatorDict[randomInt]
+        counter = counter + 1    
+
+    questionString = ''
+    counter = 0
+    while counter < 5:
+        questionString = questionString + str(operandList[counter])
+        if counter < 4:
+            questionString = questionString + str(operatorList[counter])
+        counter = counter + 1    
+
+    questionString = questionString.replace("**","^")    
+    print("\n",questionString)
+
+    userAnswer = input("Answer: ")
+
+    while True:
+        try:
+            if int(userAnswer) == eval(questionString):
+                print("Your answer was correct")
+                return(1)
+            else:
+                print("The correct answer was: " + str(eval(questionString)))
+                return(0)               
+        except:
+            print("Please enter a valid number")
+            userAnswer = input("Reenter your answer: ")
+    
+
+
